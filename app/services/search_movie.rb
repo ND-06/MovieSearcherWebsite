78bbd1fd64de3541d3e5de1f39a789ff73f095d4
Natdenice <Clad1987@hotmail.com>
2018-11-08 23:05:18 +0100

    Final commit

--- app/services/search_movie.rb
+++ app/services/search_movie.rb
@@ -1,32 +1,12 @@
-require 'themoviedb'
-
 class SearchMovie
 
-  def initialize(query)
-
+  def initialize(movie)
+    @movie = movie
     Tmdb::Api.key(Rails.application.credentials[:moviedbkey])
-    @query = query
-    @result = Hash.new
   end
 
-  def perform
-
-    @movies = Tmdb::Movie.find(@query)
-    @movies.each_with_index do |movie, index|
-      if index == 20
-        break
-      end
-      credits = Tmdb::Movie.credits(movie.id)
-      director = credits['crew'].select{ |crew| crew['department'] == 'Directing'}
-      director = director[0]['name']
-      @result[movie.id] = {
-        name: movie.title,
-        release_date: movie.release_date,
-        poster_path: movie.poster_path,
-        director: director
-      }
-    end
-    return @result
+  def result
+    Tmdb::Movie.find(@movie)
   end
 end
 
